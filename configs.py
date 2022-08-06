38f8e3edb10da1f1747992d8fc710fcf89425245
Happy9012 <110512707+Happy9012@users.noreply.github.com>
2022-08-06 19:34:03 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -15,7 +15,7 @@ class Config(object):
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
-🤖 My Name: <a href='https://t.me/FilmyFather_BotList'>Mdisk Search Robot</a>
+🤖 My Name: <a href='https://t.me/pocketmovieschannel'>Mdisk Search Robot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -23,10 +23,10 @@ class Config(object):
 
 📡 Server: <a href='https://heroku.com'>Heroku</a>
 
-👨‍💻 Created By: <a href='https://t.me/Yuvi_4502'>ʏᴜᴠʀᴀᴊ</a></b>
+👨‍💻 Created By: <a href='https://t.me/Himanshurathore0'>DON</a></b>
 """
 
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/Yuvi_4502'>ʏᴜᴠʀᴀᴊ</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/Himanshurathore0'>DON</a>
 
 If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
 """
@@ -34,22 +34,22 @@ If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
     HOME_TEXT = """
 <b>Hey! {}😅,
 
-I'm Mdisk Search Robot.🤖</a>
+I'm Lucky Mdisk Search Robot.🤖</a>
 
 I Can Search!🔍 What You Want?😜
 
-<a>Made With ❤ By @Yuvi_4502</a></b>
+<a>Made With ❤ By @Himanshurathore0</a></b>
 """
 
 
     START_MSG = """
 <b>Hey! {}😅,
 
-I'm Mdisk Search Robot.🤖</a>
+I'm Lucky Mdisk Search Robot.🤖</a>
 
 I Can Search!🔍 What You Want?😜
 
-<a>Made With ❤ By @Yuvi_4502</a></b>
+<a>Made With ❤ By @Himanshurathore0</a></b>
 """
 
 
